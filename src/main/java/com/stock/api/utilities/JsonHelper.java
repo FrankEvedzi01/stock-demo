7668c56f4284521bbd0134a7bb3262666534fb97
Frank C E <116840810+FrankEvedzi01@users.noreply.github.com>
2022-10-28 11:28:45 -0400

    Update JsonHelper.java

--- src/main/java/com/stock/api/utilities/JsonHelper.java
+++ src/main/java/com/stock/api/utilities/JsonHelper.java
@@ -263,13 +263,15 @@ public class JsonHelper {
 
 		List<StockDetails> stocks = new ArrayList<>();
 		for (String line : lines) {
-			String[] index = line.split(",");
-			StockDetails detail = new StockDetails(
-					index[0], index[1], index[2], index[3], index[4], index[5], index[6],
-					index[7], index[8], index[9], index[10], index[11], index[12], index[13], 
-					index[14], index[15]);
+			if (line.trim().length() !=0 ) {
+				String[] index = line.split(",");
+				StockDetails detail = new StockDetails(
+						index[0], index[1], index[2], index[3], index[4], index[5], index[6],
+						index[7], index[8], index[9], index[10], index[11], index[12], index[13],
+						index[14], index[15]);
 
-			stocks.add(detail);
+				stocks.add(detail);
+			}
 		}
 			
 		return stocks.stream().filter(det -> Objects.nonNull(det) && det.getStock().equalsIgnoreCase(stockValue)).toList();
