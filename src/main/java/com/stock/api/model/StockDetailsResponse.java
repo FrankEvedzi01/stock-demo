d0a4e314a4b1d4ba7ca51076d37afed1b8642789
Frank C E <116840810+FrankEvedzi01@users.noreply.github.com>
2022-10-28 01:22:03 -0400

    Update StockDetailsResponse.java

--- src/main/java/com/stock/api/model/StockDetailsResponse.java
+++ src/main/java/com/stock/api/model/StockDetailsResponse.java
@@ -2,21 +2,31 @@ package com.stock.api.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class StockDetailsResponse {
 	
+	@JsonProperty("status")
 	private String status;
 	
+	@JsonProperty("errorMessage")
+	private String errorMessage;
+	
+	@JsonProperty("details")
 	private List<StockDetails> details;
 
 	public StockDetailsResponse() {
 		
 	}
+	
 
-	public StockDetailsResponse(String status, List<StockDetails> details) {
-		
+	public StockDetailsResponse(String status, String errorMessage, List<StockDetails> details) {
+		super();
 		this.status = status;
+		this.errorMessage = errorMessage;
 		this.details = details;
 	}
+
 
 	public String getStatus() {
 		return status;
@@ -24,6 +34,14 @@ public class StockDetailsResponse {
 
 	public void setStatus(String status) {
 		this.status = status;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErroMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
 	}
 
 	public List<StockDetails> getDetails() {
@@ -34,11 +52,11 @@ public class StockDetailsResponse {
 		this.details = details;
 	}
 
+
 	@Override
 	public String toString() {
-		return "StockDetailsResponse [status=" + status + ", details=" + details + "]";
+		return "StockDetailsResponse [status=" + status + ", errorMessage=" + errorMessage + ", details=" + details + "]";
 	}
-	
-	
 
+	
 }
